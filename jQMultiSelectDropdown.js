f6791012cb9ee186fb0cd0edfa611226ba840d92
ATREYAPURAPU-SRINIVAS Sriniranjan-naga-sai-kumar <z027644-dev@corp.noxiane.net>
2019-11-18 14:20:22 +0530

    plugin files initial commit

---
+++ jQMultiSelectDropdown.js
@@ -0,0 +1,53 @@
+var dropDownSelection = [];
+$(window).on("load",function() {
+	var dropdownVal = $('#multipleDropdown').html();
+	var multiHTML = `<div class="dynamic-dropdown">
+				<div class="expand-input">
+					<div class="dynamic-elements">
+					</div>
+					<input class="search" placeholder="Please Choose a value..."/>
+				</div>
+				<ul class="search-dropdown" style="display:none;">
+				</ul>
+			</div>`;
+			$('#multipleDropdown').html(multiHTML);
+	$('#multipleDropdown .search-dropdown').html(dropdownVal);
+	$('.search').on("focus",function() {
+		$('.search-dropdown').show();
+	});
+	$('.search').on("keyup",function() {
+		var inputVal = $(this).val();
+		if (inputVal) {
+			$('.search-dropdown li').each(function() {
+				if($(this).text().toLowerCase().indexOf(inputVal.toLowerCase()) === -1) {
+					$(this).hide();
+				} else {
+					$(this).show();
+				}
+			});
+		} else {
+			$('.search-dropdown li').show();
+		}
+	});
+	$(window).on("click",function(ev) {
+		if ($(ev.target).parents('.expand-input').length === 0) {
+			$('.search-dropdown').hide();
+		}
+	});
+	$('.search-dropdown li').on("click",function() {
+		var liText = $(this).text();
+		if (dropDownSelection.indexOf(liText) === -1) {
+			dropDownSelection.push(liText);
+			$('.dynamic-elements').append('<span class="dynamic-selection">' + liText + '<span class="mark">&times;</span></span>');
+		}
+		$('.search-dropdown').hide();
+	});
+
+	$('.dynamic-elements').on("click",'.mark',function() {
+		var parentEl = $(this).parent();
+		$(parentEl).find('.mark').remove();
+		var inx = dropDownSelection.indexOf($(parentEl).text());
+		dropDownSelection.splice(inx,1);
+		$(parentEl).remove();
+	});
+});
